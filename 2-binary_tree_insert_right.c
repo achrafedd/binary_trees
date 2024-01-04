f9f39fdf9cb18108c869bc0fb1beee54b70420d4
achrafedd <achrafeddiba@gmail.com>
2024-01-04 20:38:01 +0100

    task 2: insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * 
-*/
+ * binary_tree_insert_right - insert right into binary tree
+ *
+ * @parent: parent node
+ * @value: value
+ *
+ * Return: node
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
